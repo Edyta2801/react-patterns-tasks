785a05c3c923ec9fcd49bf91c84231a42514644c
Edyta2801 <edyta.szarowska@interia.pl>
2021-01-23 13:27:46 +0100

    add Context API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,49 @@ import FancyBorder from "./FancyBorder";
 import "./App.css";
 
 
+// Context API
+
+const theme = {
+  buttonColor: "red",
+  isAuthenticated: true
+};
+
+const MyContext = React.createContext();
+
+// class ThemedButton extends React.Component {
+//   // static contextType = MyContext; // this.context
+//   render() {
+//     return (
+//       <button style={{ color: this.context.buttonColor }}>Click me</button>
+//     );
+//   }
+// }
+// // ThemedButton.contextType = MyContext; // jeśli nie mona korzystać ze static
+
+const ThemedButton = props => {
+  return (
+    <MyContext.Consumer>
+      {value =>
+        value.isAuthenticated && (
+          <button style={{ color: value.buttonColor }}>Click me</button>
+        )
+      }
+    </MyContext.Consumer>
+  );
+};
+
+const Toolbar = props => {
+  return (
+    <div>
+      <ThemedButton />
+      <ThemedButton />
+    </div>
+  );
+};
+
+
+// Composition
+
 function SpitPane(props) {
   return (
     <div>
@@ -36,6 +79,9 @@ export default function App() {
        left={<p style={{marginRight:"200px", color:"red"}}>Anna</p>}
        right={<p style={{marginLeft:"200px", color:"blue"}}>Patryk</p>}/>
       </FancyBorder>
+      <MyContext.Provider value={theme}>
+        <Toolbar />
+      </MyContext.Provider>
     </div>
   );
 }
